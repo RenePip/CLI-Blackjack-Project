46a350a6e5ae504223afea70082c14ed5055863e
renepip <renepipersburgh@aol.com>
2021-08-19 21:15:24 -0400

    Start of project

--- src/main/java/cd/rr/blackjack/BlackjackGame.java
+++ src/main/java/cd/rr/blackjack/BlackjackGame.java
@@ -1,7 +1,21 @@
 package cd.rr.blackjack;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class BlackjackGame {
     public static void main(String[] args) {
         //Start here!
+        String type;
+        int currentCard;
+        int total = 0;
+        int dealerTotal = 0;
+        int playerTotal = 0;
+        Random ran = new Random();
+        Scanner scan = new Scanner(System.in);
+
+
     }
+
+
 }
